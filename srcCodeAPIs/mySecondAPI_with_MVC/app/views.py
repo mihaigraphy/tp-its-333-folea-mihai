309e23c2b367a575e28b179bcd4a3f5b6969236c
Mihai <mihaifolea42@gmail.com>
2026-01-12 17:55:16 +0100

    Fin de scéance

--- srcCodeAPIs/mySecondAPI_with_MVC/app/views.py
+++ srcCodeAPIs/mySecondAPI_with_MVC/app/views.py
@@ -2,9 +2,19 @@ from app import app
 from flask import render_template, request, jsonify
 
 ### EXO1 - simple API
+@app.route('/')
+def index():
+    return "Simple API !"
 
 ### EXO2 - API with simple display
+@app.route('/display')
+def simple_display():
+    return render_template('index.html')
 
 ### EXO3 - API with parameters display 
+@app.route('/display_param')
+def display_param():
+    nom = "Mihai"
+    return render_template('display_param.html', nom=nom) 
 
 ### EXO4 - API with parameters retrieved from URL 
